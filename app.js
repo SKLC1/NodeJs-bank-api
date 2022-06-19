2596050a49ebccdd6789c8579387ee9414410ac5
SKLC1 <segevklein2000@gmail.com>
2022-06-19 10:27:55 +0300

    save

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
   console.log('server running on port'+ PORT);
 })
